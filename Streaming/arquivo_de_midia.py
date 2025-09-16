c1b0101c6a8918ea8a6740d7b222dfe4b8a5720f
Rafael Spumberg <rafaelspumberg31@gmail.com>
2025-09-15 21:51:06 -0300

    primeiras modificações
    readme | estruturas | arquivos de midia

---
+++ Streaming/arquivo_de_midia.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class ArquivoDeMidia(ABC):
+    def __init__(self, titulo: str, artista: str, duracao: int):
+        self.titulo = titulo
+        self.artista = artista
+        self.duracao = duracao
+        self.reproducoes = 0
+
+    @abstractmethod
+    def reproduzir(self):
+        #Simula a reprodução do arquivo de mídia
+        print(f"Reproduzindo {self.titulo} de {self.artista} ({self.duracao} minutos)")
+        self.reproducoes += 1
+
+    @abstractmethod
+    def __eq__(self, other): 
+        # Compara dois arquivos de mídia
+        return self.titulo == other.titulo and self.artista == other.artista
+    
+    def __str__(self):
+        return f"{self.titulo} por {self.artista}"
+    
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(titulo={self.titulo}, artista={self.artista})>"
