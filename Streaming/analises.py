566f410a754a3d3102f4530ad896127126d36602
arthurajvieira <arthuraraujovieira22@gmail.com>
2025-10-02 08:45:11 -0300

    commit analises

---
+++ Streaming/analises.py
@@ -0,0 +1,28 @@
+class Analises:
+    """Classe com métodos estáticos para gerar estatísticas."""
+
+    @staticmethod
+    def top_musicas_reproduzidas(musicas, top_n: int): # [cite: 75]
+        # Ordena a lista de musicas pelo atributo 'reproducoes' em ordem decrescente
+        # Retorna os top_n primeiros elementos
+        pass
+    
+    @staticmethod
+    def playlist_mais_popular(playlists): # [cite: 76]
+        # Encontra a playlist com o maior número de 'reproducoes'
+        pass
+
+    @staticmethod
+    def usuario_mais_ativo(usuarios): # [cite: 77]
+        # Encontra o usuário com o maior len(usuario.historico)
+        pass
+
+    @staticmethod
+    def media_avaliacoes(musicas): # [cite: 77]
+        # Cria um dicionário onde a chave é o título da música e o valor é a média de suas avaliações
+        pass
+
+    @staticmethod
+    def total_reproducoes(usuarios): # [cite: 78]
+        # Soma o tamanho do histórico de todos os usuários
+        pass
