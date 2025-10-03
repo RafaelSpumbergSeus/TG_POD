5f323462ffe6e94d9786b0dbfee8a35935c5ef69
Rafael Spumberg <rafaelspumberg31@gmail.com>
2025-10-02 23:50:11 -0300

    oioi

--- Streaming/analises.py
+++ Streaming/analises.py
@@ -2,27 +2,27 @@ class Analises:
     """Classe com métodos estáticos para gerar estatísticas."""
 
     @staticmethod
-    def top_musicas_reproduzidas(musicas, top_n: int): # [cite: 75]
+    def top_musicas_reproduzidas(musicas, top_n: int): 
         # Ordena a lista de musicas pelo atributo 'reproducoes' em ordem decrescente
         # Retorna os top_n primeiros elementos
         pass
     
     @staticmethod
-    def playlist_mais_popular(playlists): # [cite: 76]
+    def playlist_mais_popular(playlists): 
         # Encontra a playlist com o maior número de 'reproducoes'
         pass
 
     @staticmethod
-    def usuario_mais_ativo(usuarios): # [cite: 77]
+    def usuario_mais_ativo(usuarios): 
         # Encontra o usuário com o maior len(usuario.historico)
         pass
 
     @staticmethod
-    def media_avaliacoes(musicas): # [cite: 77]
+    def media_avaliacoes(musicas): 
         # Cria um dicionário onde a chave é o título da música e o valor é a média de suas avaliações
         pass
 
     @staticmethod
-    def total_reproducoes(usuarios): # [cite: 78]
+    def total_reproducoes(usuarios): 
         # Soma o tamanho do histórico de todos os usuários
         pass
